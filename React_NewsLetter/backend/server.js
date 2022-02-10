fafaa079cd191db208f78a669af48ec96f7dec8b
sparsh2002 <ui20ec62@iiitsurat.ac.in>
2022-02-10 18:19:08 +0530

    initial commit

---
+++ React_NewsLetter/backend/server.js
@@ -0,0 +1,52 @@
+var restify = require('restify');
+var server = restify.createServer();
+var corsMiddleware = require('restify-cors-middleware');
+var request = require('request');
+require('dotenv').config()
+function subscribe(req, res, next) {
+  var email = req.body.email; //Email entered
+  var dataCenter = process.env.DATA_CENTER;
+  var apiKey = process.env.API_KEY;
+  var listID = process.env.LIST_ID;
+  //Request settings
+  var options = {
+    url: `https://${dataCenter}.api.mailchimp.com/3.0/lists/${listID}/members`,
+    method: 'POST',
+    headers: { 'content-type': 'application/json', 'Authorization': `apikey ${apiKey}` },
+    body: JSON.stringify({ email_address: email, status: 'subscribed' })
+  }
+  // Make a simple POST call to MailChimp
+  request(options, function (error, response, body) {
+    try {
+      var respObj = {}; //Initial response object
+      if (response.statusCode === 200) {
+        respObj = { success: `Subscribed using ${email}!`, message: JSON.parse(response.body) };
+      } else {
+        respObj = { error: `Error trying to subscribe ${email}. Please try again.`, message: JSON.parse(response.body) };
+      }
+      res.send(respObj);
+    } catch (err) {
+      var respErrorObj = { error: 'There was an error with your request', message: err.message };
+      res.send(respErrorObj);
+    }
+  });
+  next();
+}
+
+//Enable CORS Middleware
+var cors = corsMiddleware({
+  origins: ['http://localhost:3000']
+});
+
+server.use(restify.plugins.bodyParser());
+
+//Use CORS...
+server.pre(cors.preflight);
+server.use(cors.actual);
+
+//Subscribe endpoint
+server.post('/subscribe', subscribe);
+
+server.listen(8080, function () {
+  console.log('%s listening at %s', server.name, server.url);
+});
---
+++ React_NewsLetter/frontend/src/App.js
@@ -0,0 +1,112 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Row, Col } from 'antd';
+import NewsletterForm from './NewsletterForm';
+import Loading from './shared/Loading';
+import axios from 'axios';
+import { apiUrl, notify } from './helpers';
+
+
+//images import
+import doodle from './assets/doodle.png'
+import logo from './assets/logo.png'
+// icon import
+// import InstagramIcon from '@mui/icons-material/Instagram';
+// import TwitterIcon from '@mui/icons-material/Twitter';
+// import FacebookIcon from '@mui/icons-material/Facebook';
+// import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      loading: false
+    }
+  }
+
+  handleLoadingState = loading => {
+    //Set loading flag
+    this.setState({ loading: loading });
+  }
+
+  handleSendEmail = email => {
+    this.handleLoadingState(true);
+    axios.post(`${apiUrl}/subscribe`, {
+      email: email
+    }).then(res => {
+      if(res.data.success) {
+        //If the response from MailChimp is good...
+        notify('success', 'Subscribed!', res.data.success);
+        this.setState({ email: '' });
+        this.handleLoadingState(false);
+      } else {
+        //Handle the bad MailChimp response...
+        notify('error', 'Unable to subscribe!', res.data.error);
+        this.handleLoadingState(false);
+      }
+    }).catch(error => {
+      //This catch block returns an error if Node API returns an error
+      notify('error', 'Error. Please try again later.', error.message);
+      this.handleLoadingState(false);
+    });
+  }
+
+  handleOnChangeEmail = email => {
+    this.setState({
+      email: email
+    })
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <div className='main-container'>
+        <div className='left-side'>
+          <img src={doodle} alt='doodle' />
+        </div>
+
+        <div className='right-side'>
+        <img src={logo} alt='logo' />
+        <h1 className='header-meow'>meow-coming soon</h1>
+        <section className="newsletter-content">
+          <Row>
+            <Col span={24}>
+              {this.state.loading
+                ? <Loading message="Working on it..." />
+                : <NewsletterForm handleSendEmail={this.handleSendEmail} handleOnChangeEmail={this.handleOnChangeEmail} email={this.state.email} />
+              }
+            </Col>
+          </Row>
+        </section>
+        <div className='text'>
+              <h4 className='subscribe'>Subscribe to stay tuned for new Updates</h4>
+              <br/>
+              <p className='main-text'>Got kitties and puppies or big fat bunnies!!! &#x1F430; </p>
+              <p className='main-text'>   We are the one stop solution for all you need for your pets!!! </p>
+              <br/>
+              <p className='main-text'>If your bunny is sick or maybe you need some dog food for your doggo,&#x1F415; </p>
+              <p className='main-text'> Have some plans for weekend but still can’t find someone to take care of your
+                 kittu &#x1F496;</p>
+              <p className='main-text'>   Or maybe you wanna connect someone with tommy!!!</p>
+              <br/>
+              <p className='main-text'> All you need is HERE!!! &#x1F607;</p>
+              <p className='main-text'>PetC : Caring loved Ones!!!</p>
+
+        <div className='newsletter-footer'>
+              Follow us on
+              <div>
+                  <a href='https://www.instagram.com/__petc__/'> Insta</a>  <a>Fb</a> <a>LinkedIn</a> <a>Twitter</a>
+              </div>
+        </div>
+        </div>
+        </div>
+        
+        </div>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
